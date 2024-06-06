6fa9b9c2b17395856b0fb0a5b0f43d18a871f5b4
Joy Paas <kjcpaas@gmail.com>
2024-06-07 03:07:01 +0800

    Unify styles

--- Take-home_Exs/Ex3/helpers/plot_centrality.R
+++ Take-home_Exs/Ex3/helpers/plot_centrality.R
@@ -1,5 +1,4 @@
 plot_centrality <- function(graph,
-                            # The network to plot
                             # Centrality measure, can be
                             # pagerank: for most powerful nodes
                             # betweeness: for power brokers
@@ -10,10 +9,12 @@ plot_centrality <- function(graph,
                             # Texts
                             title = NULL,
                             subtitle = NULL,
-                            caption = "Bigger and brighter nodes are **more influential**. *Hover on the nodes to see their full names*",
-                            arrow_margin = 1.5,
+                            caption = STYLES$default_caption,
+                            # Plot styling
+                            arrow_margin = STYLES$arrow_margin / 2,
+                            edge_thickness = STYLES$base_edge_thickness,
                             # Seed
-                            seed_num = DEFAULT_SEED) {
+                            seed_num = CONFIGS$default_seed) {
   set.seed(seed_num)
   
   if (!(centrality %in% c("pagerank", "betweenness"))) {
@@ -24,6 +25,10 @@ plot_centrality <- function(graph,
   nodes <- graph %>% activate(nodes) %>% as.data.frame()
   score_boundary <- nodes[[centrality_col]] %>% quantile(probs = 0.75)
   
+  edges <- graph %>% activate(edges) %>% as.data.frame()
+  max_weight <- edges$weight %>% max()
+  min_weight <- edges$weight %>% min()
+  
   g <- ggraph(graph, layout = layout, circular = circular) +
     # Render nodes
     geom_point_interactive(
@@ -31,123 +36,96 @@ plot_centrality <- function(graph,
         x = x,
         y = y,
         data_id = name,
-        tooltip = sprintf("%s<br/>(%s)[%0.5f]", name, subtype, .data[[centrality_col]]),
-        color = subtype,
+        tooltip = sprintf("%s(%s)<br/>Score:%0.5f", name, subtype, .data[[centrality_col]]),
         # To show people as triangle, organizations as circle
         # See scale_shape_manual code below
         shape = supertype,
-        # Get centrality measures froom a column
+        # Get centrality measures from a column
+        color = .data[[centrality_col]],
         size = .data[[centrality_col]],
-        alpha = .data[[centrality_col]]
       )
     ) +
     geom_node_text(
       aes(label = alias),
-      family = FONT_FAMILY,
-      size = 2,
-      # Nodes with low alpha have black text
-      color = ifelse(nodes[[centrality_col]] < score_boundary, "black", "white")
+      family = STYLES$font_family,
+      size = STYLES$node_label_size,
+      # Nodes with lower centrality have lighter nodes and have black text
+      color = ifelse(
+        nodes[[centrality_col]] < score_boundary,
+        STYLES$node_label_dark,
+        STYLES$node_label_light
+      )
     ) +
     
     # Render edges. Use geom_edge fan so edges along the same path don't overlap
-    geom_edge_fan (
-      aes(color = subtype),
+    geom_edge_fan(
+      aes(color = subtype, edge_width = weight),
       strength = 0.5,
-      arrow = arrow(type = "closed", length = unit(0.02, "npc")),
+      arrow = STYLES$arrow_style,
       end_cap = circle(arrow_margin, "mm"),
       start_cap = circle(arrow_margin, "mm"),
       alpha = 0.8
     ) +
-    # Person => triangle, Organization = Circle
-    scale_shape_manual(values = c("Person" = 17, "Organization" = 16)) +
-    
-    # Color schemes
-    # Colorblind pallettes from https://davidmathlogic.com/colorblind
-    scale_color_manual(
-      values = c(
-        "Person" = "#44AA99",
-        "CEO" = "#117733",
-        "Company" = "#DDCC77",
-        "FishingCompany" = "#332288",
-        "LogisticsCompany" = "#88CCEE",
-        "FinancialCompany" = "#AA4499",
-        "NGO" = "#CC6677",
-        "NewsCompany" = "#882255"
-      )
-    ) +
-    
-    # Color scheme for Edge subtype
-    scale_edge_color_manual(
-      values = c(
-        "WorksFor" = "#D81B60",
-        "Shareholdership" = "#1E88E5",
-        "BeneficialOwnership" = "#004D40",
-        "FamilyRelationship" = "#FFC107"
-      )
-    ) +
+    scale_shape_manual(values = MAPPINGS$node_supertype_to_shape) +
+    scale_edge_color_manual(values = MAPPINGS$edge_power_subtype_to_color) +
     
     # Centrality visualization
-    scale_size_continuous(range = c(2, 10), guide = FALSE) +
-    scale_alpha_continuous(range = c(0.5, 1), guide = FALSE) +
+    scale_color_gradient(
+      high = ifelse(
+        centrality == "pagerank",
+        STYLES$primary_color,
+        STYLES$secondary_color
+      ),
+      low = ifelse(
+        centrality == "pagerank",
+        STYLES$primary_light_color,
+        STYLES$secondary_light_color
+      )
+    ) +
+    scale_size_continuous(range = c(3, 12), guide = FALSE) +
+    
+    # Make sure edge widths are consistent across diff graphs
+    scale_edge_width(
+      range = c(min_weight * edge_thickness, max_weight * edge_thickness),
+      guide = "none"
+    ) +
     
     # Change legend names
-    labs(color = "Node Subtypes",
-         shape = "Node Supertypes",
-         edge_color = "Edge Subtypes") +
+    labs(
+      color = ifelse(
+        centrality == "pagerank",
+        "PageRank Score",
+        "Betweenness Score"
+      ),
+      shape = "Node Supertypes",
+      edge_color = "Edge Subtypes"
+    ) +
     
     # Make sure the plot is not clipped
-    scale_x_continuous(expand = expand_scale(mult = c(0.10, 0.10))) +
-    scale_y_continuous(expand = expand_scale(mult = c(0.10, 0.10))) +
+    scale_x_continuous(expand = expansion(mult = c(0.10, 0.10))) +
+    scale_y_continuous(expand = expansion(mult = c(0.10, 0.10))) +
     
     # Style legend keys
     guides(
-      shape = guide_legend(override.aes = list(size = 2), order = 1),
-      color = guide_legend(override.aes = list(size = 2), order = 2),
-      edge_color = guide_legend(order = 3),
+      shape = guide_legend(override.aes = list(size = 3, color = STYLES$primary_color), order = 1),
+      edge_color = guide_legend(order = 2),
+      color = guide_colorbar(order = 3)
     ) +
     
     # Style graph
     unset_graph_style() +
-    theme_graph(base_family = FONT_FAMILY, plot_margin = margin(0)) +
+    theme_graph(base_family = STYLES$font_family,
+                plot_margin = margin(0)) +
+    
     plot_annotation(title = title,
                     subtitle = subtitle,
                     caption = caption) &
-    theme(
-      text = element_text(family = FONT_FAMILY),
-      plot.title = element_text(face = "bold", size = unit(10, "pt")),
-      plot.subtitle = element_markdown(color = "grey50", size = unit(8, "pt")),
-      plot.caption = element_markdown(
-        color = "grey50",
-        size = unit(6, "pt"),
-        hjust = 0
-      ),
-      legend.position = "right",
-      legend.location = "plot",
-      legend.title = element_markdown(size = unit(8, "pt"), face = "bold"),
-      legend.text = element_markdown(size = unit(6, "pt"), vjust = 0.5),
-      legend.justification = "center",
-      legend.spacing = unit(0, "pt"),
-      legend.key.spacing = unit(1, "pt"),
-      legend.margin = margin(0),
-      legend.box.margin = margin(-28, 0, 0, 0),
-      panel.border = element_rect(
-        color = "black",
-        fill = NA,
-        size = 1
-      )
-    )
-  
-  # Add interactive elements
-  tooltip_css = paste0(
-    "background-color:black;color:white;font-family:",
-    FONT_FAMILY,
-    ";padding:4px;text-align:center;"
-  )
+    COMMON_THEME
   
   girafe(
     ggobj = g,
-    width_svg = 6,
-    height_svg = 6 * 0.618,
-    options = list(opts_tooltip(css = tooltip_css))
+    width_svg = STYLES$svg_width,
+    height_svg = STYLES$svg_height,
+    options = list(opts_tooltip(css = STYLES$tooltip_css))
   )
 }
--- Take-home_Exs/Ex3/helpers/plot_fishing_power.R
+++ Take-home_Exs/Ex3/helpers/plot_fishing_power.R
@@ -1,20 +1,19 @@
-plot_fishing_relationships <- function(# The network to plot
-  graph,
-  # Layout options
-  layout = "nicely",
-  circular = FALSE,
-  # Texts
-  title = NULL,
-  subtitle = NULL,
-  caption = "Hover on the nodes to see their full names",
-  # Plot styling
-  node_size = 7.5,
-  arrow_margin = 3.2,
-  edge_thickness = 0.15,
-  # Seed
-  seed_num = DEFAULT_SEED) {
+plot_fishing_power <- function(graph,
+                                       # The network to plot
+                                       # Layout options
+                                       layout = "nicely",
+                                       circular = FALSE,
+                                       # Texts
+                                       title = NULL,
+                                       subtitle = NULL,
+                                       caption = STYLES$default_caption,
+                                       # Plot styling
+                                       node_size = STYLES$node_size,
+                                       arrow_margin = STYLES$arrow_margin,
+                               edge_thickness = STYLES$base_edge_thickness,
+                                       # Seed
+                                       seed_num = CONFIGS$default_seed) {
   set.seed(seed_num)
-  
   edges <- graph %>% activate(edges) %>% as.data.frame()
   max_weight <- edges$weight %>% max()
   min_weight <- edges$weight %>% min()
@@ -35,47 +34,23 @@ plot_fishing_relationships <- function(# The network to plot
     ) +
     geom_node_text(
       aes(label = alias),
-      family = FONT_FAMILY,
-      size = 2,
-      color = 'white'
+      family = STYLES$font_family,
+      size = STYLES$node_label_size,
+      color = STYLES$node_label_light
     ) +
     
     # Render edges. Use geom_edge fan so edges along the same path don't overlap
-    geom_edge_fan (
+    geom_edge_fan(
       aes(color = subtype, edge_width = weight),
       strength = 0.5,
-      arrow = arrow(type = "closed", length = unit(0.02, "npc")),
+      arrow = STYLES$arrow_style,
       end_cap = circle(arrow_margin, "mm"),
       start_cap = circle(arrow_margin, "mm"),
-      alpha = 0.8,
+      alpha = 0.8
     ) +
-    # Person => triangle, Organization = Circle
-    scale_shape_manual(values = c("Person" = 17, "Organization" = 16)) +
-    
-    # Color schemes
-    # Colorblind pallettes from https://davidmathlogic.com/colorblind
-    scale_color_manual(
-      values = c(
-        "Person" = "#44AA99",
-        "CEO" = "#117733",
-        "Company" = "#DDCC77",
-        "FishingCompany" = "#332288",
-        "LogisticsCompany" = "#88CCEE",
-        "FinancialCompany" = "#AA4499",
-        "NGO" = "#CC6677",
-        "NewsCompany" = "#882255"
-      )
-    ) +
-    
-    # Color scheme for Edge subtype
-    scale_edge_color_manual(
-      values = c(
-        "WorksFor" = "#D81B60",
-        "HasShareholder" = "#1E88E5",
-        "OwnedBy" = "#004D40",
-        "FamilyRelationship" = "#FFC107"
-      )
-    ) +
+    scale_shape_manual(values = MAPPINGS$node_supertype_to_shape) +
+    scale_color_manual(values = MAPPINGS$node_subtype_to_color) +
+    scale_edge_color_manual(values = MAPPINGS$edge_power_subtype_to_color) +
     
     # Make sure edge widths are consistent across diff graphs
     scale_edge_width(
@@ -84,63 +59,35 @@ plot_fishing_relationships <- function(# The network to plot
     ) +
     
     # Change legend names
-    labs(color = "Node Subtypes",
-         shape = "Node Supertypes",
+    labs(shape = "Node Supertypes",
+         color = "Node Subtypes",
          edge_color = "Edge Subtypes") +
     
     # Make sure the plot is not clipped
-    scale_x_continuous(expand = expand_scale(mult = c(0.10, 0.10))) +
-    scale_y_continuous(expand = expand_scale(mult = c(0.10, 0.10))) +
+    scale_x_continuous(expand = expansion(mult = c(0.10, 0.10))) +
+    scale_y_continuous(expand = expansion(mult = c(0.10, 0.10))) +
     
     # Style legend keys
     guides(
-      shape = guide_legend(override.aes = list(size = 2), order = 1),
-      color = guide_legend(override.aes = list(size = 2), order = 2),
-      edges = guide_legend(order = 3)
+      shape = guide_legend(override.aes = list(size = 3, color = STYLES$primary_color), order = 1),
+      color = guide_legend(override.aes = list(size = 3), order = 2),
+      edge_color = guide_legend(order = 3),
     ) +
     
     # Style graph
     unset_graph_style() +
-    theme_graph(base_family = FONT_FAMILY, plot_margin = margin(0)) +
+    theme_graph(base_family = STYLES$font_family,
+                plot_margin = margin(0)) +
+    
     plot_annotation(title = title,
                     subtitle = subtitle,
                     caption = caption) &
-    theme(
-      text = element_text(family = FONT_FAMILY),
-      plot.title = element_text(face = "bold", size = unit(10, "pt")),
-      plot.subtitle = element_markdown(color = "grey50", size = unit(8, "pt")),
-      plot.caption = element_markdown(
-        color = "grey50",
-        size = unit(6, "pt"),
-        hjust = 0
-      ),
-      legend.position = "right",
-      legend.location = "plot",
-      legend.title = element_markdown(size = unit(8, "pt"), face = "bold"),
-      legend.text = element_markdown(size = unit(6, "pt"), vjust = 0.5),
-      legend.justification = "center",
-      legend.spacing = unit(0, "pt"),
-      legend.key.spacing = unit(1, "pt"),
-      legend.margin = margin(0),
-      legend.box.margin = margin(-28, 0, 0, 0),
-      panel.border = element_rect(
-        color = "black",
-        fill = NA,
-        size = 1
-      )
-    )
-  
-  # Add interactive elements
-  tooltip_css = paste0(
-    "background-color:black;color:white;font-family:",
-    FONT_FAMILY,
-    ";padding:4px;text-align:center;"
-  )
+    COMMON_THEME
   
   girafe(
     ggobj = g,
-    width_svg = 6,
-    height_svg = 6 * 0.618,
-    options = list(opts_tooltip(css = tooltip_css))
+    width_svg = STYLES$svg_width,
+    height_svg = STYLES$svg_height,
+    options = list(opts_tooltip(css = STYLES$tooltip_css))
   )
 }
